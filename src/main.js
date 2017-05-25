cd9faa5825ddab1f9e9cd63aae52d17152074aee
Will Stone <654103+will-stone@users.noreply.github.com>
2017-05-25 22:34:38 +0100

    cleanup

--- src/main.js
+++ src/main.js
@@ -41,9 +41,7 @@ function createWindow() {
     })
   )
 
-  // Hide dock icon
-  app.dock.hide()
-
+  // Menubar icon
   tray = new Tray(path.join(__dirname, 'images/icon/tray_iconTemplate.png'))
   tray.setPressedImage(
     path.join(__dirname, 'images/icon/tray_iconHighlight.png')
@@ -62,11 +60,11 @@ function createWindow() {
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 
+  // Hide dock icon
+  app.dock.hide()
+
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null
   })
 }
@@ -78,30 +76,7 @@ function registerNotificationListeners() {
   )
 }
 
-// Set dock icon
-// app.dock.setIcon(path.join(__dirname, 'images/icon/icon.png'))
-
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function() {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', function() {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
