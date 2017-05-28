bc86c2d5ae40e66e40e2872bce42ddbd7da033f3
Will Stone <654103+will-stone@users.noreply.github.com>
2017-05-28 11:31:07 +0100

    open spotify button

--- src/renderer.js
+++ src/renderer.js
@@ -13,6 +13,7 @@ const previous = document.getElementById('previous')
 const playPause = document.getElementById('playPause')
 const playPauseIcon = document.getElementById('playPauseIcon')
 const next = document.getElementById('next')
+const openSpotify = document.getElementById('js-open-spotify')
 
 // Components
 const art = document.getElementById('art')
@@ -24,19 +25,23 @@ const trackName = document.getElementById('trackName')
  */
 const setTrackDetails = () =>
   spotify.getTrack((err, track) => {
-    // Album art
-    art.style.backgroundImage = `url(${track.artwork_url})`
-    // Artist
-    trackArtist.innerText = track.artist
-    // Title
-    trackName.innerText = track.name
+    if (track) {
+      // Album art
+      art.style.backgroundImage = `url(${track.artwork_url})`
+      // Artist
+      trackArtist.innerText = track.artist
+      // Title
+      trackName.innerText = track.name
 
-    // Fit text to boxes
-    textFit(document.getElementsByClassName('trackDetails'), {
-      multiLine: true,
-      minFontSize: 8,
-      maxFontSize: 14
-    })
+      // Fit text to boxes
+      textFit(document.getElementsByClassName('trackDetails'), {
+        multiLine: true,
+        minFontSize: 8,
+        maxFontSize: 14
+      })
+    } else {
+      populateDetails() // this catches a bug in the notification listener when closing Spotify
+    }
   })
 
 /**
@@ -44,36 +49,53 @@ const setTrackDetails = () =>
  */
 const setState = () =>
   spotify.getState((err, state) => {
-    switch (state.state) {
-      case 'playing':
-        playPauseIcon.classList.remove('icon-play')
-        playPauseIcon.classList.add('icon-pause')
-        wrapper.classList.remove('is-paused')
-        break
-      case 'paused':
-        playPauseIcon.classList.remove('icon-pause')
-        playPauseIcon.classList.add('icon-play')
-        wrapper.classList.add('is-paused')
-        break
-      default:
-        break
+    if (state) {
+      switch (state.state) {
+        case 'playing':
+          playPauseIcon.classList.remove('icon-play')
+          playPauseIcon.classList.add('icon-pause')
+          wrapper.classList.remove('is-paused')
+          break
+        case 'paused':
+          playPauseIcon.classList.remove('icon-pause')
+          playPauseIcon.classList.add('icon-play')
+          wrapper.classList.add('is-paused')
+          break
+        default:
+          break
+      }
     }
   })
 
-// On load...
-setTrackDetails()
-setState()
+/**
+ * Populate the details for the UI. (the loader script)
+ */
+const populateDetails = () => {
+  spotify.isRunning((err, isRunning) => {
+    if (isRunning) {
+      wrapper.classList.remove('spotify-not-open')
+      setTrackDetails()
+      setState()
+    } else {
+      wrapper.classList.add('spotify-not-open')
+    }
+  })
+}
+
+// Load...
+populateDetails()
 
 // Listen for track/status changes and update
 electron.ipcRenderer.on('notification', function(event, message) {
-  setTrackDetails()
-  setState()
+  populateDetails()
 })
 
 // Bind actions to controls
 previous.addEventListener('click', () => spotify.previous())
 playPause.addEventListener('click', () => spotify.playPause())
 next.addEventListener('click', () => spotify.next())
+
+openSpotify.addEventListener('click', () => spotify.playPause())
 
 // Click to play/pause, this filters-out window drags
 // let screenX = null
