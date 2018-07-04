a7bdf0027790b18c6f17f5e08fa246790e6b0ce2
Will Stone <654103+will-stone@users.noreply.github.com>
2018-07-04 17:28:41 +0100

    add version.js
    allows for easier version updating

---
+++ docs/version.js
@@ -0,0 +1,10 @@
+const version = '3.0.1'
+
+const buttonsContainer = document.getElementById('js-buttons')
+
+const downloadButton = document.createElement('a')
+downloadButton.classList.add('button')
+downloadButton.href = `https://github.com/will-stone/SpotSpot/releases/download/v${version}/SpotSpot-${version}.dmg`
+downloadButton.innerHTML = `Download v${version}`
+
+buttonsContainer.appendChild(downloadButton)
