7c0b86823955d3d57e0607a9156674f686ac8e95
Will Stone <654103+will-stone@users.noreply.github.com>
2020-06-10 17:50:13 +0100

    Update docs version

--- docs/version.js
+++ docs/version.js
@@ -1,4 +1,4 @@
-const version = '3.3.0'
+const version = '4.0.2'
 
 const downloadButton = document.querySelector('#js-download-button')
 downloadButton.href = `https://github.com/will-stone/SpotSpot/releases/download/v${version}/SpotSpot-${version}.dmg`
